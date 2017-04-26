6cea6ec18c88f1955fd9e9434736a60c87c8f3e5
Andre-Patrick Bubel <code@andre-bubel.de>
2017-04-26 07:19:32 +0000

    Move common config to configs dir

--- configuration.nix
+++ configuration.nix
@@ -5,7 +5,7 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       ./vagrant.nix
-      ./base.nix
+      ./configs/base.nix
     ];
 
   # Use the GRUB 2 boot loader.
