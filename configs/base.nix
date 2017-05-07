0529be8ec0f2b47b03d9dbe5df0eec47d0c72a67
André-Patrick Bubel <code@andre-bubel.de>
2017-05-07 03:36:54 +0200

    Remove shell init, use nox instead of nq function

--- configs/base.nix
+++ configs/base.nix
@@ -242,24 +242,6 @@
 
   sound.mediaKeys.enable = true;
 
-  environment.interactiveShellInit = ''
-    # A nix query helper function
-    nq()
-    {
-      case "$@" in
-        -h|--help|"")
-          printf "nq: A tiny nix-env wrapper to search for packages in package name, attribute name and description fields\n";
-          printf "\nUsage: nq <case insensitive regexp>\n";
-          return;;
-      esac
-      nix-env -qaP --description \* | grep -i "$@"
-    }
-    #export HISTCONTROL=ignoreboth   # ignorespace + ignoredups
-    #export HISTSIZE=1000000         # big big history
-    #export HISTFILESIZE=$HISTSIZE
-    #shopt -s histappend             # append to history, don't overwrite it
-  '';
-
   # The NixOS release to be compatible with for stateful data such as databases.
   system.stateVersion = "17.03";
 }
