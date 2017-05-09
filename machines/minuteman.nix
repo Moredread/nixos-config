8a15801bcb8932bc54d7445d35753d6812bd8b6f
André-Patrick Bubel <code@andre-bubel.de>
2017-05-09 09:48:57 +0200

    Enable virtualbox without hardening

--- machines/minuteman.nix
+++ machines/minuteman.nix
@@ -69,7 +69,7 @@
     };
   };
 
-#  virtualisation.virtualbox.host.enable = true;
+  virtualisation.virtualbox.host.enable = true;
 #  virtualisation.virtualbox.host.enableHardening = true;
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
