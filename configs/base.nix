95d22ea1878b2116009b2ba65b471f8e929741a3
André-Patrick Bubel <code@andre-bubel.de>
2017-06-04 15:31:13 +0200

    Enable autocutsel services

--- configs/base.nix
+++ configs/base.nix
@@ -92,6 +92,7 @@
     aria2
     aspellDicts.de
     aspellDicts.en
+    autocutsel
     bashInteractive
     blender
     blink
@@ -289,6 +290,27 @@
     };
 
 
+  };
+
+  # adapted from https://gist.github.com/joedicastro/a19a9dfd21470783240c739657747f5d
+  systemd.user.services."autocutsel-clipboard" = {
+    enable = true;
+    description = "AutoCutSel for CLIPBOARD buffer";
+    wantedBy = [ "default.target" ];
+    serviceConfig.Type = "forking";
+    serviceConfig.Restart = "always";
+    serviceConfig.RestartSec = 2;
+    serviceConfig.ExecStart = "${pkgs.autocutsel}/bin/autocutsel -selection CLIPBOARD -fork";
+  };
+
+  systemd.user.services."autocutsel-primary" = {
+    enable = true;
+    description = "AutoCutSel for PRIMARY buffer";
+    wantedBy = [ "default.target" ];
+    serviceConfig.Type = "forking";
+    serviceConfig.Restart = "always";
+    serviceConfig.RestartSec = 2;
+    serviceConfig.ExecStart = "${pkgs.autocutsel}/bin/autocutsel -selection PRIMARY -fork";
   };
 
   security.dhparams.enable = true;
