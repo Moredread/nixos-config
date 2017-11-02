3fa668d0312604f1df8160ac63e634bb055e1902
André-Patrick Bubel <code@andre-bubel.de>
2017-11-02 10:44:31 +0100

    Add home-manager config

---
+++ home/home.nix
@@ -0,0 +1,40 @@
+{ pkgs, ... }:
+
+{
+  home.packages = with pkgs; [
+    apg
+    blender
+    borgbackup
+    calibre
+    evince
+    love
+    minecraft
+    mplayer
+    mpv
+    paperkey
+    python3Packages.mps-youtube
+    sloccount
+    subversion
+    youtube-dl
+    yubioath-desktop
+    vlc
+  ];
+
+  programs = {
+    home-manager.enable = true;
+    home-manager.path = https://github.com/rycee/home-manager/archive/release-17.09.tar.gz;
+
+    firefox.enable = true;
+    browserpass.enable = true;
+    htop.enable = true;
+    htop.detailedCpuTime = true;
+    htop.treeView = true;
+    lesspipe.enable = true;
+    man.enable = true;
+  };
+
+  #services.blueman-applet.enable = true;
+  #services.keepassx.enable = true;
+  #services.network-manager-applet.enable = true;
+
+}
