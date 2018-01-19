98677d629e7400ee12404cec68540f95ec0bcd4c
André-Patrick Bubel <code@apb.name>
2018-01-19 09:53:56 +0100

    Use unstable unifi

--- configs/unifi.nix
+++ configs/unifi.nix
@@ -1,9 +1,18 @@
 { config, lib, pkgs, ... }:
 
-{
+let
+  baseconfig = { allowUnfree = true; };
+  unstable = import <nixos-unstable> { config = baseconfig; };
+in {
   services.unifi = {
     enable = true;
     openPorts = true;
+  };
+
+  nixpkgs.config = {
+    packageOverrides = pkgs: {
+      unifi = unstable.unifi;
+    };
   };
 
   # Disable autostart
