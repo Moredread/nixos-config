a96dfa847065eb6836b01cf658e284e405e417c0
André-Patrick Bubel <code@andre-bubel.de>
2018-06-07 07:09:28 +0200

    Move font config to own file

---
+++ configs/fonts.nix
@@ -0,0 +1,37 @@
+{ config, lib, pkgs, ... }:
+
+{
+  fonts = {
+    enableDefaultFonts = true;
+    fonts = with pkgs; [
+      corefonts  # Microsoft free fonts
+      font-awesome_4
+      dejavu_fonts
+      fira
+      fira-mono
+      google-fonts
+      inconsolata  # monospaced
+      libertine
+      mononoki
+      overpass
+      oxygenfonts
+      powerline-fonts
+      source-code-pro
+      source-sans-pro
+      source-serif-pro
+      ttf_bitstream_vera
+      ubuntu_font_family  # Ubuntu fonts
+      unifont # some international languages
+    ];
+    fontconfig = {
+      hinting.autohint = true;
+      defaultFonts = {
+        monospace = [ "Source Code Pro" ];
+        sansSerif = [ "Source Sans Pro" ];
+        serif     = [ "Source Serif Pro" ];
+      };
+      ultimate.enable = true;
+      penultimate.enable = true;
+    };
+  };
+}
