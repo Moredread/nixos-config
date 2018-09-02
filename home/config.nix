a0ef64ea3ed09b5ffc694a50a520a35b9cd8459f
André-Patrick Bubel <code@apb.name>
2018-09-02 22:23:34 +0200

    Configure ccache wrapper

--- home/config.nix
+++ home/config.nix
@@ -1,1 +1,14 @@
-{ allowUnfree = true; }
+{
+  allowUnfree = true;
+
+      packageOverrides = pkgs: {
+       ccacheWrapper = pkgs.ccacheWrapper.override {
+         extraConfig = ''
+           export CCACHE_COMPRESS=1
+           export CCACHE_DIR=/var/cache/ccache
+           export CCACHE_UMASK=007
+         '';
+       };
+     };
+
+}
