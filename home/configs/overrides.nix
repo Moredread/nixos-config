2086d0065a2898ed49e2b151d7acf0b514b86c5e
André-Patrick Bubel <code@apb.name>
2018-09-24 07:47:46 +0200

    Use unstable for a few packages

--- home/configs/overrides.nix
+++ home/configs/overrides.nix
@@ -3,12 +3,15 @@ let
   unstable = import <nixos-unstable> {};
   overlay = self: super: {
     gitwatch = pkgs.callPackage ../pkgs/gitwatch.nix { };
+    alacritty = unstable.alacritty;
     slic3r-prusa3d = pkgs.callPackage ../pkgs/slic3r-prusa3d.nix { };
     #electrum = unstable.electrum;
-    freecad = unstable.freecad;
+    #freecad = unstable.freecad;
     syncthing = unstable.syncthing;
+    zathura = unstable.zathura;
     nixopsUnstable = unstable.nixopsUnstable;
     cachix = unstable.cachix;
+    noti = unstable.noti;
     #renoise = super.renoise.override { releasePath = /nix/store/gy1r3hkvm48qz1pvhcmvfcnscn3n49av-rns_3_1_1_linux_x86_64.tar.gz; };
   };
 in
