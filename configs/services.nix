6bd81542eeb5c7580385580c6f02d7aed8f967c6
André-Patrick Bubel <code@andre-bubel.de>
2018-10-06 17:02:07 +0200

    PS4 controller

--- configs/services.nix
+++ configs/services.nix
@@ -37,8 +37,28 @@
       ACTION=="add", SUBSYSTEM=="backlight", KERNEL=="intel_backlight", RUN+="${coreutils}/bin/chgrp video /sys/class/backlight/%k/brightness"
       ACTION=="add", SUBSYSTEM=="backlight", KERNEL=="intel_backlight", RUN+="${coreutils}/bin/chmod g+w /sys/class/backlight/%k/brightness"
 
+      # This rule is needed for basic functionality of the controller in Steam and keyboard/mouse emulation
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="28de", MODE="0666"
+      # This rule is necessary for gamepad emulation; make sure you replace 'pgriffais' with a group that the user that runs Steam belongs to
+      KERNEL=="uinput", MODE="0660", GROUP="addy", OPTIONS+="static_node=uinput"
+      # Valve HID devices over USB hidraw
+      KERNEL=="hidraw*", ATTRS{idVendor}=="28de", MODE="0666"
+      # Valve HID devices over bluetooth hidraw
+      KERNEL=="hidraw*", KERNELS=="*28DE:*", MODE="0666"
+      # DualShock 4 over USB hidraw
+      KERNEL=="hidraw*", ATTRS{idVendor}=="054c", ATTRS{idProduct}=="05c4", MODE="0666"
+      # DualShock 4 wireless adapter over USB hidraw
+      KERNEL=="hidraw*", ATTRS{idVendor}=="054c", ATTRS{idProduct}=="0ba0", MODE="0666"
+      # DualShock 4 Slim over USB hidraw
+      KERNEL=="hidraw*", ATTRS{idVendor}=="054c", ATTRS{idProduct}=="09cc", MODE="0666"
+      # DualShock 4 over bluetooth hidraw
+      KERNEL=="hidraw*", KERNELS=="*054C:05C4*", MODE="0666"
+      # DualShock 4 Slim over bluetooth hidraw
+      KERNEL=="hidraw*", KERNELS=="*054C:09CC*", MODE="0666"
+
       # NS PRO Controller USB
       KERNEL=="hidraw*", ATTRS{idVendor}=="20d6", ATTRS{idProduct}=="a711", MODE="0660", TAG+="uaccess", GROUP="input"
+
     '';
 
     # cups, for printing documents
