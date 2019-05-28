484bacf5d9db54aa60aa4870bd8dd1a1ca7858d2
André-Patrick Bubel <code@andre-bubel.de>
2019-05-28 18:26:00 +0200

    Check for renoise executable

--- home/configs/overrides.nix
+++ home/configs/overrides.nix
@@ -4,18 +4,23 @@ let
   nur = import (builtins.fetchTarball {
     url = "https://github.com/nix-community/NUR/archive/master.tar.gz";
   }) { inherit pkgs; };
+  renoisePath = ~/Downloads/rns_3_1_1_linux_x86_64.tar.gz;
   packageOverridesOverlay = self: super: {
     nur = nur;
     unstable = unstable;
 
     appimage-run = super.appimage-run.override { extraPkgs = pkgs: [ pkgs.jack2 ]; };
-    renoise = unstable.renoise.override { releasePath = ~/Downloads/rns_3_1_1_linux_x86_64.tar.gz; };
 
     i3status-rust = nur.repos.moredread.i3status-rust;
     browserpass = unstable.browserpass;
 
     pass-custom = pkgs.pass.withExtensions (ext: [ext.pass-audit ext.pass-genphrase ext.pass-update ext.pass-otp ext.pass-import]);
     lsd = unstable.lsd;
+  } // lib.filterAttrs (n: v: v != null) {
+    # TODO: can this be expressed better?
+    renoise = if builtins.pathExists renoisePath
+    then unstable.renoise.override { releasePath = renoisePath; }
+    else null;
   };
 in
 {
