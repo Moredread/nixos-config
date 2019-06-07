4175d7b591f14dc5b95c8435f19b60f873dab9e3
André-Patrick Bubel <code@andre-bubel.de>
2019-06-07 17:27:14 +0200

    Channel adder script

---
+++ scripts/channels.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+ADD="nix-channel --add"
+
+# TODO: Less redundancy
+$ADD https://github.com/rycee/home-manager/archive/release-19.03.tar.gz home-manager
+$ADD https://nixos.org/channels/nixos-19.03 nixos
+$ADD https://nixos.org/channels/nixos-unstable
+$ADD https://nixos.org/channels/nixos-19.03 nixpkgs
+
+nix-channel --update
+nix-channel --list
+
+sudo $ADD https://github.com/rycee/home-manager/archive/release-19.03.tar.gz home-manager
+sudo $ADD https://nixos.org/channels/nixos-19.03 nixos
+sudo $ADD https://nixos.org/channels/nixos-unstable
+sudo $ADD https://nixos.org/channels/nixos-19.03 nixpkgs
+
+sudo nix-channel --update
+sudo nix-channel --list
