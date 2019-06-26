7296491b1f301beca0251b3385a54a5d6d5c8478
André-Patrick Bubel <code@andre-bubel.de>
2019-06-26 06:32:05 +0200

    Fix channel script

--- scripts/channels.sh
+++ scripts/channels.sh
@@ -5,7 +5,7 @@ ADD="nix-channel --add"
 # TODO: Less redundancy
 $ADD https://github.com/rycee/home-manager/archive/release-19.03.tar.gz home-manager
 $ADD https://nixos.org/channels/nixos-19.03 nixos
-$ADD https://nixos.org/channels/nixos-unstable
+$ADD https://nixos.org/channels/nixos-unstable nixos-unstable
 $ADD https://nixos.org/channels/nixos-19.03 nixpkgs
 
 nix-channel --update
@@ -13,7 +13,7 @@ nix-channel --list
 
 sudo $ADD https://github.com/rycee/home-manager/archive/release-19.03.tar.gz home-manager
 sudo $ADD https://nixos.org/channels/nixos-19.03 nixos
-sudo $ADD https://nixos.org/channels/nixos-unstable
+sudo $ADD https://nixos.org/channels/nixos-unstable nixos-unstable
 sudo $ADD https://nixos.org/channels/nixos-19.03 nixpkgs
 
 sudo nix-channel --update
