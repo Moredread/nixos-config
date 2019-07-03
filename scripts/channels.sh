bbc24193d3f65a6222dd824645d6d413e2983667
André-Patrick Bubel <code@andre-bubel.de>
2019-07-03 18:45:09 +0200

    Refactor

--- scripts/channels.sh
+++ scripts/channels.sh
@@ -1,20 +1,21 @@
 #!/usr/bin/env bash
 
 ADD="nix-channel --add"
+VERSION="19.03"
 
 # TODO: Less redundancy
-$ADD https://github.com/rycee/home-manager/archive/release-19.03.tar.gz home-manager
-$ADD https://nixos.org/channels/nixos-19.03 nixos
+$ADD https://github.com/rycee/home-manager/archive/release-$VERSION.tar.gz home-manager
+$ADD https://nixos.org/channels/nixos-$VERSION nixos
+$ADD https://nixos.org/channels/nixos-$VERSION nixpkgs
 $ADD https://nixos.org/channels/nixos-unstable nixos-unstable
-$ADD https://nixos.org/channels/nixos-19.03 nixpkgs
 
 nix-channel --update
 nix-channel --list
 
-sudo $ADD https://github.com/rycee/home-manager/archive/release-19.03.tar.gz home-manager
-sudo $ADD https://nixos.org/channels/nixos-19.03 nixos
+sudo $ADD https://github.com/rycee/home-manager/archive/release-$VERSION.tar.gz home-manager
+sudo $ADD https://nixos.org/channels/nixos-$VERSION nixos
+sudo $ADD https://nixos.org/channels/nixos-$VERSION nixpkgs
 sudo $ADD https://nixos.org/channels/nixos-unstable nixos-unstable
-sudo $ADD https://nixos.org/channels/nixos-19.03 nixpkgs
 
 sudo nix-channel --update
 sudo nix-channel --list
