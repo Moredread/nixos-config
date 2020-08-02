dc9e76f8ad3b83698922c8e8862cebff807c0370
André-Patrick Bubel <code@apb.name>
2020-08-02 05:50:55 +0200

    Config git

--- home/configs/programs.nix
+++ home/configs/programs.nix
@@ -20,6 +20,28 @@
     noti.enable = true;
     zathura.enable = true;
 
+    git = {
+      enable = true;
+      package = pkgs.gitFull;
+      lfs.enable = true;
+      signing.key = "24B45728";
+      signing.signByDefault = true;
+      userEmail = "code@apb.name";
+      userName = "André-Patrick Bubel";
+      aliases = {
+        a = "add";
+        ca = "commit -a";
+        c = "commit";
+        co = "checkout";
+        d = "diff";
+        l = "log";
+        rh = "reset --hard";
+        s = "status";
+        t = "tag";
+        glog = "log --all --pretty='format:%d %Cgreen%h%Creset %an - %s' --graph";
+      };
+    };
+
     #    password-store = {
     #      enable = true;
     #      package = pkgs.pass.withExtensions (ext: [ext.pass-audit ext.pass-genphrase ext.pass-update ext.pass-otp ext.pass-import]);
